b21397df155aba852e6c9842268d7c58292461eb
Eragon Tang <tangyifei.tang@gmail.com>
2012-09-10 11:27:21 +0800

    初次提交

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HcmmwNew::Application.config.secret_token = 'b202ba5bd8443ab185e07bc9a4869f718fe2a76face3b0da58a15d801ee6247b736789a7487a6d552b3789ff817450323a33e9377acc6692e2654916bfef8115'
