453e821bd5f99862cfafea4c54f48c6818bd7ff5
Eragon Tang <tangyifei.tang@gmail.com>
2012-09-10 13:00:07 +0800

    添加Contact页面，写测试

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,4 +42,18 @@ describe "StaticPages" do
                                 :text => "About Us | HCMMW")
     end
   end
+
+  describe "Contact page" do
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector( 'h1',
+                                :text => 'Contact')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      page.should have_selector('title',
+                                :text => "Contact | HCMMW")
+    end
+  end
 end
