6e7cb5dc9c3888a0356229e3cdfa1d79ccbde5a8
Eragon Tang <tangyifei.tang@gmail.com>
2012-09-10 22:09:50 +0800

    将bcrypt-ruby加入Gemfile，测试Users模型的安全密码

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,19 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+
+# 用户存储到数据库前，所有邮箱地址小写字母存储
+  before_save { |user| user.email = email.downcase }
+
+# 允许邮箱地址的正则表达式
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,  presence: true, length: { maximum: 20 }
+  validates :email, presence:   true,
+                    format:     { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
 end
