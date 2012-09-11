e98015e5adddf4898794af01d65b00759bc54690
Eragon Tang <tangyifei.tang@gmail.com>
2012-09-11 10:18:08 +0800

    简化邮件地址小写函数

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
 # 用户存储到数据库前，所有邮箱地址小写字母存储
-  before_save { |user| user.email = email.downcase }
+  before_save { self.email.downcase! }
 
 # 允许邮箱地址的正则表达式
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
