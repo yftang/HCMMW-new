2452dce033d5c420355d1820477340ce42a1610a
Eragon Tang <tangyifei.tang@gmail.com>
2012-09-14 13:55:04 +0800

    基于DRY的原则，精简用户创建和修改页面的代码

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -42,14 +42,16 @@ describe "StaticPages" do
 
   it "should have the right links on the layout" do
     visit root_path
+    should_not have_link('Profile')
+    should_not have_link('Settings')
     click_link 'About'
-    page.should have_selector 'title', text: full_title('About Us')
+    should have_selector 'title', text: full_title('About Us')
     click_link 'Help'
-    page.should have_selector 'title', text: full_title('Help')
+    should have_selector 'title', text: full_title('Help')
     click_link 'Contact'
-    page.should have_selector 'title', text: full_title('Contact')
+    should have_selector 'title', text: full_title('Contact')
     click_link 'Home'
     click_link 'Sign up now!'
-    page.should have_selector 'title', text: full_title('Sign up')
+    should have_selector 'title', text: full_title('Sign up')
   end
 end
