393cc90354f8c9895fcd6d1a4a15bf7cf25f2115
Eragon Tang <tangyifei.tang@gmail.com>
2012-10-19 11:05:44 +0800

    修改实验模型文件

--- app/models/experiment.rb
+++ app/models/experiment.rb
@@ -15,12 +15,11 @@ class Experiment < ActiveRecord::Base
   attr_accessible :description, :input_file
 # belongs_to :user
   self.per_page = 10
+  mount_uploader :input_file, InputFileUploader
 
   validates :description, presence: true, length: { maximum: 40 }
   validates :input_file, presence: true
 # validates :user_id, presence: true
 
   default_scope order: 'experiments.created_at DESC'
-
-  mount_uploader :input_file, InputFileUploader
 end
